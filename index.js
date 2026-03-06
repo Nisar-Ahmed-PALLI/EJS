ef685caf118ac9bc5b2b3e4f24f052eab96f8dd1
nisar ahmed <nisarpalli96@gmail.com>
2026-03-07 01:09:23 +0500

    ejs files

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const port =  8080;
+
+const path = require("path");
+
+app.set("view Engine","ejs");
+app.set("views", path.join(__dirname,"/views"));
+
+app.get("/home", (req,res) =>{
+     res.render("home.ejs");
+     console.log("Request Received");
+});
+
+app.get("/cal", (req,res) =>{
+     res.render("cal.ejs");
+     console.log("Request Received");
+});
+
+app.listen(port,(req,res)=>{
+   console.log(`app is listening ${port}`);
+});
+
+app.get("/apple", (req,res)=>{
+     res.send("this is for demo");
+     console.log("Request Received");
+});
